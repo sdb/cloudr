a35f4ddbe60db75f6aeba8a02dc04a1a41b2c775
Stefan De Boey <stefan@ellefant.be>
2011-11-02 15:54:41 +0100

    remove integration tests

--- tests/src/main/scala/TestApplication.scala
+++
@@ -1,9 +0,0 @@
-package be.ellefant.droid.cloudapp
-package tests
-
-import android.content.Context
-import android.app.Application
-
-class TestApplication(context: Context) extends Application {
-  attachBaseContext(context)
-}
