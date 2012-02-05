3fa443369ddd4ba22dc0a852fb961263643dc665
Stefan De Boey <stefan@ellefant.be>
2012-02-05 20:41:00 +0100

    more specs

--- app/test/be/ellefant/cloudr/DropActivitySpec.scala
+++ app/test/be/ellefant/cloudr/DropActivitySpec.scala
@@ -1,8 +1,6 @@
 package be.ellefant.cloudr
 
 import android.accounts.Account
-import com.xtremelabs.robolectric.Robolectric._
-import CloudAppManager._
 import java.util.Date
 import android.widget.{CheckBox, TextView}
 import sdroid.robolectric.TestMenuItem
@@ -10,14 +8,20 @@ import android.content.{ContextWrapper, Intent}
 import com.xtremelabs.robolectric.Robolectric._
 import android.net.Uri
 import com.xtremelabs.robolectric.shadows.ShadowToast
-import Cloud._
+import Cloud._, CloudAppManager._
 
 class DropActivitySpec extends CloudrSpecification { def is = sequential ^
   "DropActivity should" ^
     "show the drop details" ! Context().displayDetails ^
     "open the drop in a browser" ! Context().openBrowser ^
-    "delete the drop" ! Context().delete ^
-    "not delete the drop with auth error" ! Context().deleteAuthError
+    "delete the drop" ! DeleteContext().withSuccess ^
+    "not delete the drop with auth error" ! DeleteContext().withAuthError ^
+    "not delete the drop with error" ! DeleteContext().withError ^
+    "restore the drop" ! RestoreContext().withSuccess ^
+    "not restore the drop with auth error" ! RestoreContext().withAuthError ^
+    "not restore the drop with error" ! RestoreContext().withError ^
+    "listen to content changes via the observer" ! pending ^
+    "show menus" ! pending
   end
 
   case class Context() extends RoboContext
@@ -69,42 +73,91 @@ class DropActivitySpec extends CloudrSpecification { def is = sequential ^
     }
 
     def openBrowser = testMenuAction(R.id.browse, new Intent(Intent.ACTION_VIEW, Uri.parse(drop.url)))
-    
-    def delete = this {
+  }
+  
+  case class DeleteContext() extends ApiContext {
+    val apiCall = cloudAppMock delete _
+    val expectedToastMessage = "This item will be removed."
+    val actionId = R.id.delete
+  }
+
+  case class RestoreContext() extends ApiContext {
+    val apiCall = cloudAppMock recover _
+    val expectedToastMessage = "This item will be restored."
+    val actionId = R.id.restore
+  }
+
+  abstract class ApiContext extends RoboContext
+      with Mocks.AccountManagerMock
+      with Mocks.CloudAppMock
+      with Mocks.DropManagerMock
+      with Bindings.ThreadUtilBinding {
+
+    val acc = new Account("sdb", AccountType)
+    accountManagerMock.getAccountsByType(AccountType) returns Array(acc)
+    accountManagerMock.getPassword(acc) returns "blabla"
+
+    val drop = Drop(0, "", "", "", true, true, "", ItemType.Bookmark, 0, "", None, None, "", new Date, new Date, None)
+    dropManagerMock.find(0) returns (Some(drop))
+
+    val activity = new DropActivity
+
+    val apiCall: String => Either[Error.Error, Drop]
+    def expectedToastMessage: String
+    def actionId: Int
+
+    def withSuccess = this {
       val intent = new Intent()
       intent.putExtra(KeyId, drop.id)
       shadowOf(activity) setIntent intent
       activity onCreate null
 
       (accountManagerMock blockingGetAuthToken (acc, AuthTokenType, true)) returns ("blabla")
-      (cloudAppMock.delete(drop.href)) returns (Right(drop))
-      
-      val menuItem = TestMenuItem(R.id.delete)
+      (apiCall(drop.href)) returns (Right(drop))
+
+      val menuItem = TestMenuItem(actionId)
       activity.onOptionsItemSelected(menuItem)
 
-      ShadowToast.getTextOfLatestToast must be_==("This item will be removed.") and {
+      ShadowToast.getTextOfLatestToast must be_==(expectedToastMessage) and {
         there was one(dropManagerMock).update(drop)
       }
     }
 
-    def deleteAuthError = this {
+    def withAuthError = this {
       val intent = new Intent()
       intent.putExtra(KeyId, drop.id)
       shadowOf(activity) setIntent intent
       activity onCreate null
 
       (accountManagerMock blockingGetAuthToken (acc, AuthTokenType, true)) returns ("blabla")
-      (cloudAppMock.delete(drop.href)) returns (Left(Error.Auth))
+      (apiCall(drop.href)) returns (Left(Error.Auth))
 
-      val menuItem = TestMenuItem(R.id.delete)
+      val menuItem = TestMenuItem(actionId)
       activity.onOptionsItemSelected(menuItem)
 
-      ShadowToast.getTextOfLatestToast must be_==("This item will be removed.") and {
+      ShadowToast.getTextOfLatestToast must be_==(expectedToastMessage) and {
         there was no(dropManagerMock).update(drop)
       } and {
         there was one(accountManagerMock).clearPassword(acc)
       } and {
         there was one(accountManagerMock).invalidateAuthToken(AccountType, "blabla")
+      }
+    }
+
+    def withError = this {
+      val intent = new Intent()
+      intent.putExtra(KeyId, drop.id)
+      shadowOf(activity) setIntent intent
+      activity onCreate null
+
+      (accountManagerMock blockingGetAuthToken (acc, AuthTokenType, true)) returns ("blabla")
+      (apiCall(drop.href)) returns (Left(Error.Other))
+
+      val menuItem = TestMenuItem(actionId)
+      activity.onOptionsItemSelected(menuItem)
+
+      ShadowToast.getTextOfLatestToast must be_==(expectedToastMessage) and {
+        there was no(dropManagerMock).update(drop)
       }
     }
   }
